d6a7701968b4395146d04899052acb297d195a1a
Mosher23 </var/mail/pi>
2022-05-15 21:17:18 +0200

    backup 2022-05-15

--- auto_mcu_update.sh
+++ auto_mcu_update.sh
@@ -11,4 +11,8 @@ make clean KCONFIG_CONFIG=config.rpi
 make KCONFIG_CONFIG=config.rpi
 make flash KCONFIG_CONFIG=config.rpi
 
+make clean KCONFIG_CONFIG=config.toolhead
+make KCONFIG_CONFIG=config.toolhead
+python3 lib/canboot/flash_can.py -i can0 -f ./out/klipper.bin -u beb382378295
+
 sudo systemctl start klipper
